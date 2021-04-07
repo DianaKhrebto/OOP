1d4f072766cdedc06cb04e8138f058cbe928d878
DianaKhrebto <82101006+DianaKhrebto@users.noreply.github.com>
2021-04-07 20:31:26 +0700

    1Lab

---
+++ Header.h
@@ -0,0 +1,77 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Класс "растение"
+class Plant {
+    string Name; //Название растения
+public:
+    string Get_Name(); //Функция получения названия растения
+
+    static Plant* In_Plant(ifstream& ifst); //Функция вывод растения
+    virtual void In_Data(ifstream& ifst) = 0; //Чисто вирутальная функция ввода растения,
+                                              //она будет определена каждого класса растения
+    virtual void Out_Data(string Name, ofstream& ofst) = 0; //Чисто вирутальная функция вывода растения,
+                                              //она будет определена каждого класса растения
+protected:
+    Plant() {};
+};
+
+//Класс "дерево"
+class Tree : public Plant {
+    long int Age; //Возраст дерева
+public:
+    void In_Data(ifstream& ifst); //Функция ввода дерева
+    void Out_Data(string Name, ofstream& ofst); //Функция вывода дерева
+    Tree() {};
+};
+
+//Класс "кустарник"
+class Shrub : public Plant {
+    //Месяц цветения
+    enum Month {
+        JENUARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER
+    };
+
+    Month M; //Месяц цветения
+public:
+    void In_Data(ifstream& ifst); //Функция ввода кустарника
+    void Out_Data(string Name, ofstream& ofst); //Функция вывода кустарника
+    Shrub() {};
+};
+
+//Структура "узел контейнера"
+struct Node {
+    Node* Next; //Указатель на следующий элемент контейнера (узел)
+    Plant* Cont; //Указатель на растение
+};
+
+class Container {
+    Node* Head; //Указатель на первый элемент контейнера
+    int Len; //Количество элементов в контейнере
+public:
+    void In_Container(ifstream& ifst); //Функция ввода элементов в контейнер
+    void Out_Container(ofstream& ofst); //Функция вывода контейнера
+    void Clear_Container(); //Функция очистки контейнера
+    Container(); //Конструктор
+    ~Container() { Clear_Container(); } //Деструктор  
+};
+
+#endif // HEADER_H
---
+++ Source.cpp
@@ -0,0 +1,163 @@
+#include "Header.h"
+
+Container::Container() {
+    Head = new Node();
+    Head->Cont = NULL;
+    Head->Next = NULL;
+}
+
+void Container::In_Container(ifstream& ifst) {
+    Node* Temp_Node = Head;
+    
+    while (!ifst.eof()) {
+        if ((Temp_Node->Cont = Plant::In_Plant(ifst))) {
+            Temp_Node->Next = new Node();
+            Temp_Node = Temp_Node->Next;
+            Len++;
+        }
+    }
+}
+
+void Container::Out_Container(ofstream& ofst) {
+    ofst << "Container contains " << Len
+        << " elements." << endl << endl;
+
+    Node* Temp_Node = Head;
+
+    for (int i = 0; i < Len; i++) {
+        ofst << i << ": ";
+        Temp_Node->Cont->Out_Data(Temp_Node->Cont->Get_Name(), ofst);
+        Temp_Node = Temp_Node->Next;
+    }
+}
+
+void Container::Clear_Container() {
+    for (int i = 0; i < Len; i++) {
+        free(Head->Cont);
+        Head = Head->Next;
+    }
+
+    Len = 0;
+}
+
+string Plant::Get_Name() {
+    return Name;
+}
+
+Plant* Plant::In_Plant(ifstream& ifst) {
+    Plant* P;
+    int K;
+
+    ifst >> K;
+    if (K == 1) {
+        P = new Tree;
+    }
+    else if (K == 2) {
+        P = new Shrub;
+    }
+    else {
+        return 0;
+    }
+
+    ifst >> P->Name;
+
+    P->In_Data(ifst);
+    
+    return P;
+}
+
+void Tree::In_Data(ifstream& ifst) {
+    ifst >> Age;
+}
+
+void Shrub::In_Data(ifstream& ifst) {
+    string Month = "";
+
+    ifst >> Month;
+
+    if (Month == "Jenuary") {
+        M = JENUARY;
+    }
+    else if (Month == "February") {
+        M = FEBRUARY;
+    }
+    else if (Month == "March") {
+        M = MARCH;
+    }
+    else if (Month == "April") {
+        M = APRIL;
+    }
+    else if (Month == "May") {
+        M = MAY;
+    }
+    else if (Month == "June") {
+        M = JUNE;
+    }
+    else if (Month == "July") {
+        M = JULY;
+    }
+    else if (Month == "August") {
+        M = AUGUST;
+    }
+    else if (Month == "September") {
+        M = SEPTEMBER;
+    }
+    else if (Month == "October") {
+        M = OCTOBER;
+    }
+    else if (Month == "November") {
+        M = NOVEMBER;
+    }
+    else if (Month == "December") {
+        M = DECEMBER;
+    }
+}
+
+void Tree::Out_Data(string Name, ofstream& ofst) {
+    ofst << "It's a tree with name: " << Name << endl;
+    ofst << "Tree's age is " << Age << endl << endl;
+}
+
+void Shrub::Out_Data(string Name, ofstream& ofst) {
+    ofst << "It's a shrub with name: " << Name << endl;
+    ofst << "Shrub's flowering month is ";
+
+    if (M == JENUARY) {
+        ofst << "Jenuary";
+    }
+    else if (M == FEBRUARY) {
+        ofst << "February";
+    }
+    else if (M == MARCH) {
+        ofst << "March";
+    }
+    else if (M == APRIL) {
+        ofst << "April";
+    }
+    else if (M == MAY) {
+        ofst << "May";
+    }
+    else if (M == JUNE) {
+        ofst << "June";
+    }
+    else if (M == JULY) {
+        ofst << "July";
+    }
+    else if (M == AUGUST) {
+        ofst << "August";
+    }
+    else if (M == SEPTEMBER) {
+        ofst << "September";
+    }
+    else if (M == OCTOBER) {
+        ofst << "October";
+    }
+    else if (M == NOVEMBER) {
+        ofst << "November";
+    }
+    else if (M == DECEMBER) {
+        ofst << "December";
+    }
+
+    ofst << endl << endl;
+}
